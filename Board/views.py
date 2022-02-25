a6d4f81c1e2598a1720a37825a937aed214aa825
zake <tjtjdguq77@naver.com>
2022-02-26 03:25:58 +0900

    feb.26th pagination update in ChartList

--- Board/views.py
+++ Board/views.py
@@ -4,7 +4,7 @@ from .forms import LyricInsertForm
 from .web_scraping import get_billboard100
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login
+from django.core.paginator import Paginator
 # Create your views here.
 def getHome(request):
     return render(request,"Board/index.html")
@@ -18,7 +18,10 @@ def refreshBillBoard(request):
 
 def getSongList(request):
     songs=Songs.objects.all()
-    return render(request,"Board/ChartList.html",{'billboard':songs})
+    paginator=Paginator(songs,10)
+    page_num=request.GET.get('page')
+    page_obj=paginator.get_page(page_num)
+    return render(request,"Board/ChartList.html",{'billboard':page_obj,'range':range(1,page_obj.paginator.num_pages)})
 
 def getSongDetail(request,rank):
     song=Songs.objects.get(rank=rank)
